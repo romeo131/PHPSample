2509b21bf74bb01e327331c1960871d34aacab65
MrKarl <romeo131@gmail.com>
2015-06-23 14:52:27 -0700

    bug fix

--- db.php
+++ db.php
@@ -7,7 +7,7 @@ ini_set("display_errors", 1);
 
 
 	$db_host = "localhost";
-	$db_user = "root";
+	$db_user = "administrator";
 	$db_password = "netmarble1!";
 	$db_name = "test";
 	$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
@@ -15,14 +15,13 @@ ini_set("display_errors", 1);
 	if (mysqli_connect_errno()) {
 		echo "DB connect fail : ".mysqli_connect_error();
 	} else {
-		echo "DB Connected <br>"
-		
+		echo "DB Connected <br>";
 			$rs = mysqli_query($conn, "select * from tb_sample");
 		
-		while($row = mysql_fetch_array($rs)) {
-			echo $row['name'];
-			echo "<br>";
-		}
+			while($row = mysql_fetch_array($rs)) {
+				echo $row['name'];
+				echo "<br>";
+			}
 	};
 
 	mysqli_close($conn);
