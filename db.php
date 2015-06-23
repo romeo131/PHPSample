b819b8a815f30ae4e5febac8e4f53a33b7759abd
MrKarl <romeo131@gmail.com>
2015-06-23 14:36:32 -0700

    connect to mysql

--- db.php
+++ db.php
@@ -1,4 +1,25 @@
 <?php
-	echo 'hello world';
+	echo 'hello world<br>';
+
+
+	$db_host = "localhost";
+	$db_user = "root";
+	$db_password = "netmarble1!";
+	$db_name = "test";
+	$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 	
+	if (mysqli_connect_errno()) {
+		echo "DB connect fail : ".mysqli_connect_error();
+	} else {
+		echo "DB Connected <br>"
+		
+			$rs = mysqli_query($conn, "select * from tb_sample");
+		
+		while($row = mysql_fetch_array($result)) {
+			echo $row['name'];
+			echo "<br>";
+		}
+	};
+
+	mysqli_close($conn);
 ?>
