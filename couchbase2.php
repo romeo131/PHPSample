c4bb9d1832c21e5a70a282ce94f233dbd1771a77
MrKarl <romeo131@gmail.com>
2015-06-23 18:51:24 -0700

    .

--- couchbase2.php
+++ couchbase2.php
@@ -1,21 +1,20 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 // Connect to Couchbase Server
-$cluster = new CouchbaseCluster("http://192.168.1.18:8091", "Administrator", "netmarble1!", "beer-sample");
 
+$cluster = new CouchbaseCluster('http://127.0.0.1:8091');
 $bucket = $cluster->openBucket('beer-sample');
 
 // Retrieve a document
+
 $result = $bucket->get('aass_brewery-juleol');
 $doc = $result->value;
 
-//echo $doc->name.', ABV: '.$doc->abv."\n";
-echo $doc->name;
+echo $doc->name . ', ABV: ' . $doc->abv . "\n";
 
 // Store a document
+
 $doc->comment = 'Random beer from Norway';
+
 $result = $bucket->replace('aass_brewery-juleol', $doc);
 
 var_dump($result);
