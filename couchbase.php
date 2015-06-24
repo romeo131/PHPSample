ed43efa44887da760a89e67337024f39605b1351
MrKarl <romeo131@gmail.com>
2015-06-24 11:45:44 -0700

    .

--- couchbase.php
+++ couchbase.php
@@ -2,28 +2,24 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 //[CouchbaseNative]/CouchbaseBucket.class.php:74\n
-// Connect to Couchbase Server
 //$cluster = new CouchbaseCluster('http://192.168.1.18:8091');
 //$cluster = new CouchbaseCluster('couchbase://localhost');
-echo "1<br>";
+
 $cluster = new CouchbaseCluster('http://127.0.0.1:8091');
-echo "2<br>";
 $bucket = $cluster->openBucket('beer-sample');
-echo "3<br>";
-var_dump($bucket);
+//var_dump($bucket);
+
 // Retrieve a document
 $result = $bucket->get('aldaris');
-echo "4<br>";
 $doc = $result->value;
-echo "5<br>";
 var_dump($doc);
 //echo $doc->name.', ABV: '.$doc->abv."\n";
+
 echo "goforit : ".$doc->name;
 echo "6<br>";
 // Store a document
 $doc->comment = 'Random beer from Norway';
-echo "7<br>";
 $result = $bucket->replace('aldaris', $doc);
-echo "8<br>";
+
 var_dump($result);
 ?>
